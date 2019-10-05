13ebd1a9e306f0dd8d6dd571cff47f3a2711f1ed
nataliChavez <science_tec12@hotmail.com>
2019-10-05 14:23:38 -0500

    contador inicio

---
+++ Contador/contador_app/lib/main.dart
@@ -0,0 +1,9 @@
+
+import 'package:contador_app/src/app.dart';
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(new MyApp());
+}
+
---
+++ Contador/contador_app/lib/src/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget
+{
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+        home:Center(
+            child:
+            Text("Hola mundo")
+        )
+    );
+  }
+
+}
